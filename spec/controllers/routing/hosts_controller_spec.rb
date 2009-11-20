4502c9d6906e3a0c479b15d48547e999df4a6096
Rick Bradley <rick@rickbradley.com>
2009-11-20 16:43:34 +0530

    making the hosts index be the default page, for now

--- spec/controllers/routing/hosts_controller_spec.rb
+++ spec/controllers/routing/hosts_controller_spec.rb
@@ -2,4 +2,8 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe HostsController, 'routing' do
   it_should_behave_like 'a RESTful controller with routes'
+  
+  it "should map the application root to the hosts index" do
+    params_from(:get, "/").should == {:controller => "hosts", :action => "index"}
+  end
 end
