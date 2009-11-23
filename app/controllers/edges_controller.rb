0c4d4b18c31d2bd2030c6bd0ce5ea910ad7f7b88
Rick Bradley <rick@rickbradley.com>
2009-11-23 21:50:20 +0530

    EdgesController#link -> EdgesController#create

--- app/controllers/edges_controller.rb
+++ app/controllers/edges_controller.rb
@@ -1,5 +1,17 @@
 class EdgesController < ApplicationController
-  resources_controller_for :edge, :except => [ :destroy, :link ]
+  
+  # POST /edges?edge[source_id]=1&edge[target_id]=2
+  def create
+    @source = Service.find(params[:edge][:source_id])
+    @target = Service.find(params[:edge][:target_id])
+    @edge = Edge.create!(:source => @source, :target => @target)
+    
+    respond_to do |format|
+      format.html { redirect_to service_path(@source) }
+      format.js
+      format.xml  { render :xml => @edge, :status => :created, :location => edge_url(@edge) }
+    end
+  end
 
   # DELETE /edge/1
   # DELETE /edge/1.xml
@@ -11,19 +23,6 @@ class EdgesController < ApplicationController
       format.html { redirect_to service_path(@source) }
       format.js
       format.xml  { head :ok }
-    end
-  end
-  
-  # POST /edges/link?source_id=1&target_id=2
-  def link
-    @source = Service.find(params[:source_id])
-    @target = Service.find(params[:target_id])
-    @edge = Edge.create!(:source => @source, :target => @target)
-    
-    respond_to do |format|
-      format.html { redirect_to service_path(@source) }
-      format.js
-      format.xml  { render :xml => @edge, :status => :created, :location => edge_url(@edge) }
     end
   end
 end
--- spec/controllers/edges_controller_spec.rb
+++ spec/controllers/edges_controller_spec.rb
@@ -7,58 +7,38 @@ describe EdgesController, 'when integrating' do
     @edge = Edge.generate!
   end
 
-  describe 'index' do
-    def do_request
-      get :index
-    end
-
-    it_should_behave_like "a successful action"
-  end
-
-  describe 'new' do
-    def do_request
-      get :new
-    end
-
-    it_should_behave_like "a successful action"
-  end
-
-  describe 'show' do
-    def do_request
-      get :show, :id => @edge.id.to_s
-    end
-
-    it_should_behave_like "a successful action"
-  end
-
-  describe 'edit' do
-    def do_request
-      get :edit, :id => @edge.id.to_s
-    end
-
-    it_should_behave_like "a successful action"
-  end
-
   describe 'create' do
     before :each do
-      @edge = Edge.spawn
+      @source, @target = Service.generate!, Service.generate!
+      @source_id, @target_id = @source.id.to_s, @target.id.to_s
     end
     
-    def do_request
-      post :create, :edge => @edge.attributes
+    it 'should fail when no source_id is provided' do
+      lambda { post :create, :edge => { :target_id => @target_id } }.should raise_error      
     end
 
-    it_should_behave_like "a redirecting action"
-  end
-
-  describe 'update' do
-    def do_request
-      put :update, :id => @edge.id.to_s, :edge => @edge.attributes
+    it 'should fail when no target_id is provided' do
+      lambda { post :create, :edge => { :source_id => @source_id } }.should raise_error
     end
-
-    it_should_behave_like "a redirecting action"
+    
+    it 'should fail when an invalid source_id is provided' do
+      lambda { post :create, :edge => { :source_id => (@source.id+100).to_s, :target_id => @target_id } }.should raise_error      
+    end
+    
+    it 'should fail when an invalid target_id is provided' do
+      lambda { post :create, :edge => { :source_id => @source_id, :target_id => (@target.id+100).to_s } }.should raise_error            
+    end
+    
+    it 'should create a new edge from the source to the target' do
+      lambda { post :create, :edge => { :source_id => @source_id, :target_id => @target_id } }.should change(Edge, :count)
+    end
+    
+    it 'should redirect to the source show page' do
+      post :create, :edge => { :source_id => @source_id, :target_id => @target_id }
+      response.should redirect_to(service_path(@source))
+    end
   end
-
+  
   describe 'destroy' do
     before(:each) do
       @edge = Edge.generate!
@@ -84,45 +64,4 @@ describe EdgesController, 'when integrating' do
       response.should redirect_to(service_path(source))
     end
   end
-  
-  describe 'link' do
-    before :each do
-      @source, @target = Service.generate!, Service.generate!
-      @source_id, @target_id = @source.id.to_s, @target.id.to_s
-    end
-    
-    it 'should fail when no source_id is provided' do
-      lambda { post :link, :target_id => @target_id }.should raise_error      
-    end
-
-    it 'should fail when no target_id is provided' do
-      lambda { post :link, :source_id => @source_id }.should raise_error
-    end
-    
-    it 'should fail when an invalid source_id is provided' do
-      lambda { post :link, :source_id => (@source.id+100).to_s, :target_id => @target_id }.should raise_error      
-    end
-    
-    it 'should fail when an invalid target_id is provided' do
-      lambda { post :link, :source_id => @source_id, :target_id => (@target.id+100).to_s }.should raise_error            
-    end
-    
-    it 'should create a new edge from the source to the target' do
-      lambda { post :link, :source_id => @source_id, :target_id => @target_id }.should change(Edge, :count)
-    end
-    
-    it 'should redirect to the source show page' do
-      post :link, :source_id => @source_id, :target_id => @target_id
-      response.should redirect_to(service_path(@source))
-    end
-  end
-end
-
-describe EdgesController, 'when not integrating' do
-  it_should_behave_like 'a RESTful controller with an index action'
-  it_should_behave_like 'a RESTful controller with a show action'
-  it_should_behave_like 'a RESTful controller with a new action'
-  it_should_behave_like 'a RESTful controller with a create action'
-  it_should_behave_like 'a RESTful controller with an edit action'
-  it_should_behave_like 'a RESTful controller with an update action'
 end
